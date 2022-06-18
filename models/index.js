9ed8ac76726a1317819d3baff6f3ef7682b69fc7
JeremyGashu <gearmias@gmail.com>
2022-06-18 15:15:06 +0300

    added init

--- models/index.js
+++ models/index.js
@@ -9,6 +9,7 @@ const config = require(__dirname + '/../config/config')[env];
 const db = {};
 
 let sequelize;
+console.log('Ermias Gashu', require(__dirname + '/../config/config')[env])
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
